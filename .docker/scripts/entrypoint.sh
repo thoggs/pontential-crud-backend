f8d0a96e34d388c1097442f10384af04febb8855
Thiago Rodrigues <thiagothogs@icloud.com>
2024-06-18 11:13:50 -0300

    feat: added configuration for apache and entrypoint

---
+++ .docker/scripts/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Wait for dependencies to be ready
+sleep 10s
+
+# Ensure all necessary storage directories exist and have the correct permissions
+mkdir -p /var/www/html/storage/framework/views /var/www/html/public/storage/logs
+touch /var/www/html/public/storage/logs/laravel.log
+chown -R www-data:www-data /var/www/html/storage /var/www/html/public/storage
+chmod -R 775 /var/www/html/storage /var/www/html/public/storage
+
+# Debugging: List storage directory
+ls -la /var/www/html/storage
+ls -la /var/www/html/storage/framework/views
+ls -la /var/www/html/public/storage
+ls -la /var/www/html/public/storage/logs
+
+# Run Laravel migrations and seeders
+php artisan migrate
+php artisan db:seed --class=DevelopersSeeder
+
+# Start Apache server
+exec apache2-foreground
