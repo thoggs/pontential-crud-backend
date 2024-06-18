39828aba3f874672bdfc77f601e902e9449dddb5
Thiago Rodrigues <thiagothogs@icloud.com>
2024-06-18 12:03:25 -0300

    feat: added new uuid middleware validation

---
+++ app/Http/Middleware/ValidateUUID.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+
+class ValidateUUID
+{
+    public function handle(Request $request, Closure $next): ResponseAlias
+    {
+        $id = $request->route('developer');
+
+        if ($id && !Uuid::isValid($id)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => [
+                    'errors' => 'Invalid ID format'
+                ],
+                'model' => (array())
+            ], ResponseAlias::HTTP_BAD_REQUEST);
+        }
+
+        return $next($request);
+    }
+}
